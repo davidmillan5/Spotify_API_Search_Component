4ec113987fec8ba641ead8c6dfa8004c0d22a346
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-04-30 09:06:45 -0500

    Searcher component Finished

--- react_searcher/src/App.jsx
+++ react_searcher/src/App.jsx
@@ -16,6 +16,7 @@ const CLIENT_SECRECT = '4913008ece8f45a9913114fc98f5f1bd';
 function App() {
   const [searchInput, setSearchInput] = useState('');
   const [accessToken, setAccessToken] = useState('');
+  const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
     // API Access Token
@@ -41,7 +42,7 @@ function App() {
     console.log('Search for ' + searchInput);
 
     // Get Request using search to get the Artist ID
-    var artistParameters = {
+    var searchParameters = {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -50,12 +51,28 @@ function App() {
     };
     var artistID = await fetch(
       'https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist',
-      artistParameters
+      searchParameters
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
-    // Get request with Artist ID grab all the albums from the artist
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
 
+    console.log('Artist ID is ' + artistID);
+    // Get request with Artist ID grab all the albums from the artist
+    var returnedAlbums = await fetch(
+      'https://api.spotify.com/v1/artists/' +
+        artistID +
+        '/albums' +
+        '?include_groups=album&market=US&limit=50',
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAlbums(data.items);
+      });
+    console.log(albums);
     // Display Those Albums to The Users
   }
 
@@ -78,42 +95,17 @@ function App() {
       </Container>
       <Container>
         <Row className="mx-2 row row-cols-4">
-          <Card className="m-3">
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
-          <Card className="m-3">
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
-          <Card className="m-3">
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
-          <Card className="m-3">
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
-          <Card className="m-3">
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
-          <Card className="m-3">
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
+          {albums.map((album, i) => {
+            console.log(album);
+            return (
+              <Card key={album.id}>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </Row>
       </Container>
     </>
